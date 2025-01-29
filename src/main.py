f98db95c408d6a6615c7401fbe7a3343c99afe61
Ana Silva <anacxx@live.com>
2025-01-28 23:07:34 -0300

    gitignore atualizado

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from utils.gantt_utils import load_tasks, group_tasks_by_group, plot_gantt
 
 def main():
-    file_path = 'data/tasks.csv'
+    file_path = 'data/project_tasks.xlsx'
     tasks = load_tasks(file_path)
     grouped_tasks = group_tasks_by_group(tasks)
     plot_gantt(grouped_tasks)
